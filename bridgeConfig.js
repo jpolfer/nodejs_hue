1e6077997120d122669efdf942ca7bf6ee776364
Jon Polfer <jonathan.polfer@gmail.com>
2018-02-09 07:05:42 -0600

    added additional hue code control

---
+++ bridgeConfig.js
@@ -0,0 +1,29 @@
+var HueApi = require("node-hue-api").HueApi;
+
+var displayResult = function(result) {
+    console.log(JSON.stringify(result, null, 2));
+};
+
+var host = "192.168.1.76",
+    username = "s5le-cIUhAfl3VC77G7L5kOYKFFO8OqgKhh0MaEm",
+    api;
+
+api = new HueApi(host, username);
+
+// --------------------------
+// Using a promise
+api.config().then(displayResult).done();
+// using getConfig() alias
+api.getConfig().then(displayResult).done();
+
+// --------------------------
+// Using a callback
+api.config(function(err, config) {
+    if (err) throw err;
+    displayResult(config);
+});
+// using getConfig() alias
+api.getConfig(function(err, config) {
+    if (err) throw err;
+    displayResult(config);
+});
---
+++ registerUser.js
@@ -0,0 +1,4 @@
+// USE the philips hue raw api tool to create a user
+
+// visit http://<hue bridge ip>/debug/clip.html
+// make a POST to /api with {"devicetype":"<name of user here>"} as message body to create a new user
